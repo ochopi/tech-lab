8d97330816ed02b5db62516fe8699d81d4521faf
ochopi <53490489+ochopi@users.noreply.github.com>
2026-06-16 22:40:02 +0000

    reorganizacion

--- BK-Repairs-Old/Ver Fecha Expiracion Windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-slmgr -xpr
-exit
--- pendiente/oobe/desatendido.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Aplicando configuraciones desatendidas... 
-setlocal
-set XML=%~dp0autounattend.xml
-if not exist "%WINDIR%\Panther" mkdir "%WINDIR%\Panther"
-copy /Y "%XML%" "%WINDIR%\Panther\unattend.xml" >nul
-timeout /t 3 >nul
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:%XML% /reboot
-exit
